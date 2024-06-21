6ad6bde038a5c9db09b8b41a895f6ce9b00f7dc6
Brandon Castillo <brajoel98@gmail.com>
2024-06-20 23:30:07 -0700

    feat(components): :technologist: update to textarea

--- src/modules/core/components/form/InputForm.tsx
+++ src/modules/core/components/form/InputForm.tsx
@@ -4,9 +4,15 @@ interface InputFormProps {
   name: string;
   label: string;
   type?: string;
+  textarea?: boolean;
 }
 
-export const InputForm = ({ name, label, type = 'text' }: InputFormProps) => {
+export const InputForm = ({
+  name,
+  label,
+  type = 'text',
+  textarea = false,
+}: InputFormProps) => {
   return (
     <>
       <label htmlFor={name} className="block text-sm font-medium text-gray-300">
@@ -14,6 +20,7 @@ export const InputForm = ({ name, label, type = 'text' }: InputFormProps) => {
       </label>
       <Field
         id={name}
+        as={textarea && 'textarea'}
         name={name}
         type={type}
         className="block w-full px-3 py-2 border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 bg-gray-700"
