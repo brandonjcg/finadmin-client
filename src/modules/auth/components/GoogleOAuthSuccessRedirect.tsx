75fe25199db2c40ec0b31179fa9a7bcad1c486c0
Brandon Castillo <brajoel98@gmail.com>
2024-09-29 22:58:48 -0700

    feat: :lock: add protect routes with jwt (#47)

--- src/modules/auth/components/GoogleOAuthSuccessRedirect.tsx
+++ src/modules/auth/components/GoogleOAuthSuccessRedirect.tsx
@@ -2,15 +2,18 @@ import { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
 import { UserDef, useAuthState } from '../state';
+import { useCookies } from 'react-cookie';
 
 export const GoogleOAuthSuccessRedirect = () => {
   const [params] = useSearchParams();
   const { setUser } = useAuthState();
   const navigate = useNavigate();
+  const [, setCookie] = useCookies(['jwt']);
 
   useEffect(() => {
     const jwtUser = params.get('jwtUser');
     if (jwtUser) {
+      setCookie('jwt', jwtUser);
       const userFromJwt: UserDef = jwtDecode(jwtUser);
       userFromJwt && setUser(userFromJwt);
     }
